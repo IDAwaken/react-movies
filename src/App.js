a1961da24cd9d51022040bbdc7b04c3cf6d89743
Revatto <revattojs@gmail.com>
2020-08-23 22:07:44 +0100

    App.js from function to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
