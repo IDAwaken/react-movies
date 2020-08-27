c1360eab6532057f52088ea7bb3b23c1191766e7
Revatto <revattojs@gmail.com>
2020-08-27 22:14:45 +0100

    Added About component link

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mt-3">
+      <h3>About This App</h3>
+      <h5>App to search for movies</h5>
+      <h6>Version 1.0.0</h6>
+    </div>
+  );
+};
+
+export default About;
