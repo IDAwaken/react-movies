6a3920fd58c25dbe05cece431be5969cf791357e
Revatto <revattojs@gmail.com>
2020-08-28 20:14:56 +0100

    ul added with two Links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,6 +7,18 @@ const Navbar = () => {
       <Link to="/" className="navbar-brand text-light">
         React Movies
       </Link>
+      <ul className="nav ml-auto">
+        <li className="nav-item">
+          <Link to="/" className="nav-link text-light">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link text-light">
+            About
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
