271bae0c03ee218b35ddddbf68f55c2d4ec6b776
Revatto <revattojs@gmail.com>
2020-08-30 18:03:41 +0100

    Changed MovieDetails Class component to Functional component with useEffect hook

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -1,77 +1,81 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 import Reviews from "../reviews/Reviews";
 
-class MovieDetails extends Component {
-  componentDidMount() {
-    this.props.getMovie(this.props.match.params.id);
-    this.props.getMovieReviews(this.props.match.params.id);
-  }
+const MovieDetails = ({
+  movie,
+  loading,
+  getMovie,
+  getMovieReviews,
+  match,
+  reviews,
+}) => {
+  useEffect(() => {
+    getMovie(match.params.id);
+    getMovieReviews(match.params.id);
+    // eslint-disable-next-line
+  }, []);
 
-  render() {
-    const {
-      vote_average,
-      title,
-      release_date,
-      overview,
-      poster_path,
-      genres = [],
-    } = this.props.movie;
+  const {
+    vote_average,
+    title,
+    release_date,
+    overview,
+    poster_path,
+    genres = [],
+  } = movie;
 
-    const { loading, reviews } = this.props;
-
-    if (loading) {
-      return <Spinner />;
-    } else {
-      return (
-        <div className="row my-2 mx-0">
-          <div className="col-12 col-sm-12 p-0">
-            <Link to="/" className="btn bg-info text-light mb-2 btn-sm">
-              <i className="fa fa-arrow-left" /> Back To Search
-            </Link>
-            <div className="card">
-              <div className="row m-0">
-                <div className="col-4 mx-auto p-0">
-                  <img
-                    src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-                    alt=""
-                    style={{
-                      width: "100%",
-                    }}
-                  />
-                </div>
-                <div className="col-sm-12 col-md-8 col-lg-8">
-                  <div className="card-body">
-                    <div className="card-title">
-                      <h4>{title}</h4>
-                      <p>Release Date: {release_date}</p>
-                      <p>{overview}</p>
-                      <p>Rate: {vote_average}/10</p>
-                      <div>
-                        Genres:
-                        {genres.map((genre) => (
-                          <span
-                            className="badge badge-danger ml-2"
-                            key={genre.id}
-                            genre={genre}
-                          >
-                            {genre.name}
-                          </span>
-                        ))}
-                      </div>
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="row my-2 mx-0">
+        <div className="col-12 col-sm-12 p-0">
+          <Link to="/" className="btn bg-info text-light mb-2 btn-sm">
+            <i className="fa fa-arrow-left" /> Back To Search
+          </Link>
+          <div className="card">
+            <div className="row m-0">
+              <div className="col-4 mx-auto p-0">
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+                  alt=""
+                  style={{
+                    width: "100%",
+                  }}
+                />
+              </div>
+              <div className="col-sm-12 col-md-8 col-lg-8">
+                <div className="card-body">
+                  <div className="card-title">
+                    <h4>{title}</h4>
+                    <p>Release Date: {release_date}</p>
+                    <p>{overview}</p>
+                    <p>Rate: {vote_average}/10</p>
+                    <div>
+                      Genres:
+                      {genres.map((genre) => (
+                        <span
+                          className="badge badge-danger ml-2"
+                          key={genre.id}
+                          genre={genre}
+                        >
+                          {genre.name}
+                        </span>
+                      ))}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <Reviews reviews={reviews} />
         </div>
-      );
-    }
+
+        <Reviews reviews={reviews} />
+      </div>
+    );
   }
-}
+};
 
 export default MovieDetails;
