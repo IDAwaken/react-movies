0eb0f56c48a9d62bd10829da025698b6de2b69b9
Revatto <revattojs@gmail.com>
2020-08-30 17:33:23 +0100

    Changed Search Class component to Functional component with useState

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Search extends Component {
-  state = {
-    text: "",
-  };
+const Search = ({ searchMovies, clearMovies, showClearBtn, setAlert }) => {
+  // state = {
+  //   text: "",
+  // };
+
+  const [text, setText] = useState("");
 
   // Update the state when submitting the search
-  onSubmit = (e) => {
-    if (this.state.text === "") {
+  const onSubmit = (e) => {
+    if (text === "") {
       e.preventDefault();
-      this.props.setAlert("Please enter a movie title", "danger");
+      setAlert("Please enter a movie title", "danger");
     } else {
       e.preventDefault();
-      this.props.searchMovies(this.state.text);
-      this.setState({ text: "" });
+      searchMovies(text);
+      setText("");
     }
   };
 
   // Tracking input in search
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setText(e.target.value);
   };
 
-  render() {
-    const { showClearBtn, clearMovies } = this.props;
-
-    return (
-      <div className="row mt-3">
-        <div className="col-12">
-          <form className="form" onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                name="text"
-                placeholder="Search a movie title..."
-                className="form-control w-100 offset md-6 mb-2"
-                value={this.state.text}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="submit"
-                className="btn btn-block bg-info text-light"
-                value="Search"
-                onSubmit={this.onSubmit}
-              />
-            </div>
-            {showClearBtn && (
-              <button
-                type="submit"
-                value="Clear"
-                className="btn btn-block bg-dark text-light"
-                onClick={clearMovies}
-              >
-                Clear
-              </button>
-            )}
-          </form>
-        </div>
+  return (
+    <div className="row mt-3">
+      <div className="col-12">
+        <form className="form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="text"
+              placeholder="Search a movie title..."
+              className="form-control w-100 offset md-6 mb-2"
+              value={text}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              className="btn btn-block bg-info text-light"
+              value="Search"
+              onSubmit={onSubmit}
+            />
+          </div>
+          {showClearBtn && (
+            <button
+              type="submit"
+              value="Clear"
+              className="btn btn-block bg-dark text-light"
+              onClick={clearMovies}
+            >
+              Clear
+            </button>
+          )}
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Search;
