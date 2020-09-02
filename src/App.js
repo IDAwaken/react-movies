75c5a22de76907f973593d183a5e9fb6cdcc7c95
Revatto <revattojs@gmail.com>
2020-09-02 20:11:07 +0100

    Added a Home component that wraps Search and Movies components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Search from "./components/layout/Search";
+// import Search from "./components/layout/Search";
 import Alert from "./components/layout/Alert";
-import Movies from "./components/movies/Movies";
+// import Movies from "./components/movies/Movies";
 import MovieDetails from "./components/movies/MovieDetails";
 import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 import MovieState from "./context/movies/MovieState";
 import AlertState from "./context/alert/AlertState";
@@ -38,18 +40,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <>
-                      <Search />
-                      <Movies />
-                    </>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/movie/:id" component={MovieDetails} />
+                <Route component={NotFound} className="mt-3" />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "../layout/Search";
+import Movies from "../movies/Movies";
+
+const Home = () => {
+  return (
+    <>
+      <Search />
+      <Movies />
+    </>
+  );
+};
+
+export default Home;
