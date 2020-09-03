975380bcf00e165be41d29c878aad6a49a14010c
Revatto <revattojs@gmail.com>
2020-09-03 09:43:05 +0100

    Changed the card style of the MovieItem component

--- src/components/movies/MovieItem.js
+++ src/components/movies/MovieItem.js
@@ -4,30 +4,29 @@ import { Link } from "react-router-dom";
 const MovieItem = ({ movie: { title, id, poster_path, release_date } }) => {
   return (
     <div className="card-group">
-      <div className="card text-center">
-        <div className="card-img-top">
-          {poster_path == null ? (
-            <img
-              src="https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg"
-              alt="poster-icon"
-              style={{ width: "100%" }}
-            />
-          ) : (
-            <img
-              src={`https://image.tmdb.org/t/p/w185/${poster_path}
+      <div className="card text-center d-flex align-items-stretch justify-content-center">
+        <Link to={`/movie/${id}`}>
+          <div className="card-img-top">
+            {poster_path == null ? (
+              <img
+                src="https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg"
+                alt="poster-icon"
+                style={{ width: "100%" }}
+              />
+            ) : (
+              <img
+                src={`https://image.tmdb.org/t/p/w185/${poster_path}
             `}
-              alt="poster-icon"
-            />
-          )}
-        </div>
-        <div className="card-body d-flex align-items-center justify-content-center">
-          <span className="card-title card-text-size m-0">{title}</span>
-        </div>
+                alt="poster-icon"
+              />
+            )}
+          </div>
+          <div className="card-body d-flex align-items-center justify-content-center">
+            <span className="card-title m-0">{title}</span>
+          </div>
+        </Link>
         <div className="card-footer">
-          <p>{release_date}</p>
-          <Link to={`/movie/${id}`} className="btn btn-dark bg-info text-white">
-            View Details
-          </Link>
+          {release_date ? <p>{release_date}</p> : <p>No Date</p>}
         </div>
       </div>
     </div>
