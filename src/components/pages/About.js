fa4ea2388d2aa8253c8bd275d574c0386ea9e64c
Revatto <revattojs@gmail.com>
2020-09-08 18:07:17 +0100

    Edited About component content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,11 @@ const About = () => {
   return (
     <div className="mt-3">
       <h3>About This App</h3>
-      <h5>App to search for movies</h5>
+      <h5>
+        App to search for movies using from{" "}
+        <a href="https://developers.themoviedb.org/">themoviedb API</a> made by{" "}
+        <a href="https://github.com/aminebelmahi">Amine Belmahi</a>
+      </h5>
       <h6>Version 1.0.0</h6>
     </div>
   );
