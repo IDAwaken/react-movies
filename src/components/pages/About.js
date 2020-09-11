5ee5aebc7e2bc566f605f102b1babcb6feaf2986
Revatto <revattojs@gmail.com>
2020-09-11 20:30:50 +0100

    Edited About component &  index.html file
    git commit -m "Edited About component &  index.html file"

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const About = () => {
   return (
-    <div className="mt-3">
+    <div className='mt-3'>
       <h3>About This App</h3>
       <h5>
-        App to search for movies using from{" "}
-        <a href="https://developers.themoviedb.org/">themoviedb API</a> made by{" "}
-        <a href="https://github.com/aminebelmahi">Amine Belmahi</a>
+        App to search for movies using from{' '}
+        <a href='https://developers.themoviedb.org/'>themoviedb API</a>
       </h5>
+      <h6>
+        Made by <a href='https://github.com/aminebelmahi'>Amine Belmahi</a>
+      </h6>
       <h6>Version 1.0.0</h6>
     </div>
   );
